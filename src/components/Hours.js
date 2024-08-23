02ec4b73b957a2174bfb6002e1656a0fc8060625
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2024-08-23 09:59:44 +0000

    update-app

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,23 @@
-// Placeholder to host a component
+import React from 'react';
+
+const Hours = () => {
+    const currentDay = new Date().getDay();
+    let openingHours;
+
+    if (currentDay >= 1 && currentDay <= 5) {
+        openingHours = '10 a.m. - 4 p.m.';
+    } else if (currentDay === 0 || currentDay === 6) {
+        openingHours = '9 a.m. - 8 p.m.';
+    } else {
+        openingHours = 'Closed';
+    }
+
+    return (
+        <div>
+            <h2>Shelter Hours</h2>
+            <p>Today's hours: {openingHours}</p>
+        </div>
+    );
+};
+
+export default Hours;
